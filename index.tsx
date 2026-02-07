57958ae8d3366de050ebc55cb56ef79ba37cbd12
Mavis <38875846+xychenxy@users.noreply.github.com>
2026-02-07 11:25:03 +1100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,6 @@
-
+npx @openapitools/openapi-generator-cli generate \
+  -i https://your-api/swagger/v1/swagger.json \
+  -g typescript-axios \
+  -o src/api/generated \
+  --global-property=models,modelDocs=false,modelTests=false \
+  --additional-properties=supportsES6=true
